734dde311ce511273dfd56437a4686a1531d534c
Chris Finney <the.finney@gmail.com>
2017-03-24 08:34:59 -0700

    add backend logic and pass first two tests - instantiate Member object correctly and with member name

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,3 +1,14 @@
-public class Member {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Member {
+  private String mMemberName;
+
+  public Member(String memberName) {
+  mMemberName = memberName;
+  }
+
+  public String getMemName() {
+    return mMemberName;
+  }
 }
--- src/test/java/MemberTest.java
+++ src/test/java/MemberTest.java
@@ -3,4 +3,15 @@ import static org.junit.Assert.*;
 
 public class MemberTest {
 
+  @Test
+  public void Member_instantiatesCorrectly_true() {
+    Member member = new Member("Aaron");
+    assertEquals(true, member instanceof Member);
+  }
+
+  @Test
+  public void getMemName_instantiatesWithMemberName_String() {
+    Member member = new Member("Aaron");
+    assertEquals("Aaron", member.getMemName());
+  }
 }
